8ae95b6015315f9c3a6c48e6c8dbd0905bc6f194
bingo8670 <307286130@qq.com>
2017-11-29 16:44:17 +0800

    add BMI calculator

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  get "/hello_world", to: "pages#hello"
+  get  "bmi",          to: "bmi#index"
+  post "bmi/result",   to: "bmi#result"
+
+  get  "/hello_world", to: "pages#hello"
   resources :posts
   resources :users
 end
